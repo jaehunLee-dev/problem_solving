f4fee46662b748f607070b1b80f405fbd4933c12
jaehunLee-dev <jaehunlee.dev@gmail.com>
2022-06-12 19:43:06 +0900

    1783

--- 1783.cc
+++ 1783.cc
@@ -7,17 +7,10 @@ int main(){
     if (M>=7 && N>=3){
         ans+=4;
         midx=7;
-    }
-    else{       //4번 미만으로 움직일때
-        for (int i=0; i<4; i++){
+        while (1){
             if (nidx+2 <= N && midx+1 <= M){
                 nidx+=2;
                 midx+=1;
-                ans++;
-            }
-            else if (nidx+1 <= N && midx+2 <= M){
-                nidx+=1;
-                midx+=2;
                 ans++;
             }
             else if (nidx-2 >= 1 && midx+1 <= M){
@@ -25,6 +18,11 @@ int main(){
                 midx+=1;
                 ans++;
             }
+            else if (nidx+1 <= N && midx+2 <= M){
+                nidx+=1;
+                midx+=2;
+                ans++;
+            }            
             else if (nidx-1 >= 1 && midx+2 <= M){
                 nidx-=1;
                 midx+=2;
@@ -33,8 +31,34 @@ int main(){
             else 
                 break;                            
         }
-        cout << ans;
-        return 0;
     }
+    else{       //4번 미만으로 움직일때
+        printf("this");
+        for (int i=0; i<3; i++){
+            if (nidx+2 <= N && midx+1 <= M){
+                nidx+=2;
+                midx+=1;
+                ans++;                
+            }
+            else if (nidx-2 >= 1 && midx+1 <= M){
+                nidx-=2;
+                midx+=1;
+                ans++;
+            }
+            else if (nidx+1 <= N && midx+2 <= M){
+                nidx+=1;
+                midx+=2;
+                ans++;
+            }            
+            else if (nidx-1 >= 1 && midx+2 <= M){
+                nidx-=1;
+                midx+=2;
+                ans++;
+            }
+            else 
+                break;                            
+        }
+    }
+    cout << ans;
     return 0;
 }
