d004960ea4a4dc869d79620730be791d14ce16f4
jaehunLee-dev <jaehunlee.dev@gmail.com>
2022-06-27 23:22:56 +0900

    easy

---
+++ stepbystep/25083.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "         ,r'\"7\n"
+         << "r`-_   ,'  ,/\n"
+         << " \\. \". L_r'\n"
+         << "   `~\\/\n"
+         << "      |\n"
+         << "      |\n";
+    return 0;
+}
