a1b7f202ee6a055d803ab9c0f98b0c56cda6c914
jaehunLee-dev <jaehunlee.dev@gmail.com>
2022-07-05 02:08:24 +0900

    stack

--- stack/10773.cc
+++ stack/10773.cc
@@ -2,6 +2,21 @@
 #include <stack>
 using namespace std;
 int main(){
-    
+    stack<int> s;
+    int n; cin >> n;
+    int tmp;
+    for (int i=0; i<n; i++){
+        cin >> tmp;
+        if (tmp == 0)
+            s.pop();
+        else
+            s.push(tmp);
+    }
+    tmp = 0;
+    while (!s.empty()){
+        tmp += s.top();
+        s.pop();
+    }
+    cout << tmp;
     return 0;
 }
