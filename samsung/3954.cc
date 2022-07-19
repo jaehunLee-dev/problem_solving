d68a37f3a7d031188a5465eea4957cbd6debe768
jaehunLee-dev <jaehunlee.dev@gmail.com>
2022-07-19 13:21:55 +0900

    tmp

---
+++ samsung/3954.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int test_case; cin >> test_case;
+    while (test_case--){
+        int s_m, s_c, s_i;
+        cin >> s_m >> s_c >> s_i;
+        string cmd;
+        cin >> cmd;
+    }
+    return 0;
+}
