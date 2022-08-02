4c9dad28a118647ad177570ebbb0c59b6735c317
jaehunLee-dev <jaehunlee.dev@gmail.com>
2022-08-03 00:20:50 +0900

    window

--- samsung/15684.cc
+++ samsung/15684.cc
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
+int N, M, H;
 int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> N >> M >> H;
+    bool** ladder = new bool*[N+1]; //ladder[2][1] : 2번째 가로에서 1->2로 가는 것
+    for (int i=1; i<H; i++)
+        ladder[i] = new bool[H+1]();
+    for (int i=0; i<M; i++){
+        int a, b;
+        cin >> a >> b;
+        ladder[a][b] = true;        //a번째 가로에서 a->a+1 가는 것
+    }
 
     return 0;
 }
+//
