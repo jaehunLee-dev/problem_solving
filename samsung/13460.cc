80ec5b852f0adf6b7a0de70c762d67823a1bd674
jaehun <jaehun@jaehunui-MacBookAir.local>
2022-08-05 18:44:31 +0900

    bfs,조건 까다로움

--- samsung/13460.cc
+++ samsung/13460.cc
@@ -1,7 +1,138 @@
 #include <iostream>
+#include <queue>
 using namespace std;
+int ans = 11;
+int N, M;
+pair<int,int> r_idx;
+pair<int,int> b_idx;
+//아래,오른,위,왼
+int dy[4] = {1,0,-1,0};
+int dx[4] = {0,1,0,-1};
+char** board;
 int main(){
     ios::sync_with_stdio(false);
-    if ()
-    return 0;    
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> N >> M;
+    queue< pair < pair< int, int > , int > > red_q;    //좌표, 횟수
+    queue< pair < pair< int, int > , int > > blue_q;    //좌표, 횟수
+    board = new char*[N];
+    for (int i=0; i<N; i++)
+        board[i] = new char[M];    
+    for (int i=0; i<N; i++){
+        for (int j=0; j<M; j++){
+            cin >> board[i][j];
+            if (board[i][j] == 'R'){
+                r_idx.first = i;
+                r_idx.second = j;
+                board[i][j] = '.';
+            }
+            else if (board[i][j] == 'B'){
+                b_idx.first = i;
+                b_idx.second = j;
+                board[i][j] = '.';
+            }                
+        }
+    }
+    red_q.push(make_pair(r_idx,0));
+    blue_q.push(make_pair(b_idx,0));
+    while(!red_q.empty()){                
+        int y = red_q.front().first.first;
+        int x = red_q.front().first.second;
+        int num = red_q.front().second;
+        red_q.pop();
+        int blue_y = blue_q.front().first.first;
+        int blue_x = blue_q.front().first.second;
+        
+        blue_q.pop();
+        if (board[blue_y][blue_x] == 'O' || num>=10) continue;        
+        for (int i=0; i<4; i++){
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            int bny = blue_y + dy[i];
+            int bnx = blue_x + dx[i];            
+            if (board[ny][nx] != '#'){
+                while(board[ny][nx] == '.'){
+                    ny += dy[i];
+                    nx += dx[i];                        
+                }                               
+                if (board[bny][bnx] != '#'){
+                    while(board[bny][bnx] == '.'){
+                        bny += dy[i];
+                        bnx += dx[i];
+                    }
+                    if (board[bny][bnx] == '#'){
+                        bny -= dy[i];
+                        bnx -= dx[i];                                
+                    }
+                }
+                else {
+                    bny = blue_y;
+                    bnx = blue_x;
+                }                
+
+                if (board[ny][nx] == '#'){
+                    ny -= dy[i];
+                    nx -= dx[i];
+                    if (ny == bny && nx == bnx){
+                        int r_abs = abs(y - ny) + abs(x-nx);
+                        int b_abs = abs(blue_y - bny) + abs(blue_x - bnx);
+                        if (r_abs > b_abs){
+                            ny -= dy[i];
+                            nx -= dx[i];
+                        }
+                        else{
+                            bny -= dy[i];
+                            bnx -= dx[i];
+                        }                            
+                    }
+                    if (board[bny][bnx] == 'O')
+                        continue;
+                    pair<int,int> idx;
+                    idx.first = ny; 
+                    idx.second = nx;                        
+                    red_q.push(make_pair(idx,num+1));                        
+                    idx.first = bny;
+                    idx.second = bnx;
+                    blue_q.push(make_pair(idx,num+1));
+                }
+                else if (board[ny][nx] == 'O'){
+                    if (board[bny][bnx] != 'O'){    //red가 먼저 들어감
+                        cout << num+1;
+                        exit(0);
+                    }                        
+                }
+            }         
+            
+            else if (board[bny][bnx] != '#'){
+                //red 불가, blue만 가능한 경우
+                while(board[bny][bnx] == '.'){
+                    bny += dy[i];
+                    bnx += dx[i];
+                }
+                if (board[bny][bnx] == '#'){
+                    bny -= dy[i];
+                    bnx -= dx[i];                                
+                }
+                if (bny == y && bnx == x){
+                    bny -= dy[i];
+                    bnx -= dx[i];
+                }            
+                if (board[bny][bnx] == 'O')
+                    continue;
+                pair<int,int> idx;
+                idx.first = y;
+                idx.second = x;
+                red_q.push(make_pair(idx,num+1));
+                idx.first = bny;
+                idx.second = bnx;
+                blue_q.push(make_pair(idx,num+1));
+            } 
+            
+        }
+    }
+    if (ans > 10) ans = -1;    
+    cout << ans;
+    return 0;
 }
+
+//빨간색이 못가고 파란색이 갈 수 있는 방향도 해줘야하네....
--- samsung/tempCodeRunnerFile.cc
+++ samsung/tempCodeRunnerFile.cc
@@ -1,76 +1,129 @@
 #include <iostream>
+#include <queue>
 using namespace std;
-int N, M, H;
-bool** ladder;
-int ans = 4;
-inline bool do_ladder(){
-    for (int l_num = 1; l_num <=N; l_num++){
-        int now = l_num;
-        for (int i=1; i<=H; i++){
-            if (ladder[i][now])
-                now = now+1;            
-            else if (ladder[i][now-1])
-                now = now - 1;
-        }
-        if (now != l_num){
-            return false;
-        }
-    }
-    return true;    
-}
-void comb(int sero, int garo, int idx){    //1. 가로로 확장 2. 세로로 확장 (왼->오, 위->아래)
-    if (ans == 0){
-        cout << ans;
-        exit(0);
-    }
-    if (idx > 3)    //정답은 3보다 작아야한다.
-        return;
-    if (garo >= N){
-        garo = 1;
-        sero +=1;
-    }
-    if (idx >= ans) return;
-    if (do_ladder()){
-        if (idx < ans){            
-            ans = idx;
-            return; //이미 사다리 완성이면 return
-        }
-    }    
-    //if (idx >= ans) return;
-    if (garo > N || sero > H) return;
-
-    if (ladder[sero][garo]){    //원래 사다리가 있으면, 확인 없이 그냥 바로 다다음 사다리로        
-        comb(sero,garo+2,idx);        
-    }
-    //시간복잡도 줄이는 법 -> 이전꺼는 확인 안하기(보장하기) -> 현재꺼에 사다리추가하면 바로 +2번째로, 사다리 추가안하면 +1번째로
-    else{
-        if (!ladder[sero][garo+1]){
-            ladder[sero][garo] = true;            
-            comb(sero,garo+2,idx+1);            
-            ladder[sero][garo] = false;            
-            comb(sero,garo+1,idx);                                        
-        }
-        else{       //다음꺼가 사다리일때 -> 다다다음꺼 or 다음줄            
-            comb(sero,garo+3,idx);            
-        }
-    }    
-    //사다리 추가하는 작업 후 항상 do_ladder로 완성됐는지 체크
-}
+int ans = 11;
+int N, M;
+pair<int,int> r_idx;
+pair<int,int> b_idx;
+//아래,오른,위,왼
+int dy[4] = {1,0,-1,0};
+int dx[4] = {0,1,0,-1};
+char** board;
+bool** r_visited;   //빨간 공 방문 확인
 int main(){
     ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);    
-    cin >> N >> M >> H;
-    ladder = new bool*[H+2]; //ladder[2][1] : 2번째 가로에서 1->2로 가는 것
-    for (int i=0; i<H+2; i++)
-        ladder[i] = new bool[N+2]();
-    for (int i=0; i<M; i++){
-        int a, b;
-        cin >> a >> b;
-        ladder[a][b] = true;        //a번째 가로에서 b->b+1 가는 것
-    }    
-    comb(1,1,0);
-    if (ans == 4)
-        ans = -1;
-    cout << ans;    
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> N >> M;
+    queue< pair < pair< int, int > , int > > red_q;    //좌표, 횟수
+    queue< pair < pair< int, int > , int > > blue_q;    //좌표, 횟수
+    board = new char*[N];
+    r_visited = new bool*[N];
+    for (int i=0; i<N; i++){
+        board[i] = new char[M];
+        r_visited[i] = new bool[M]();
+    }
+    for (int i=0; i<N; i++){
+        for (int j=0; j<M; j++){
+            cin >> board[i][j];
+            if (board[i][j] == 'R'){
+                r_idx.first = i;
+                r_idx.second = j;
+                r_visited[i][j] = true;
+                board[i][j] = '.';
+            }
+            else if (board[i][j] == 'B'){
+                b_idx.first = i;
+                b_idx.second = j;
+                board[i][j] = '.';
+            }                
+        }
+    }
+    red_q.push(make_pair(r_idx,0));
+    blue_q.push(make_pair(b_idx,0));
+    while(!red_q.empty()){                
+        int y = red_q.front().first.first;
+        int x = red_q.front().first.second;
+        int num = red_q.front().second;
+        red_q.pop();
+        int blue_y = blue_q.front().first.first;
+        int blue_x = blue_q.front().first.second;
+        
+        blue_q.pop();
+        if (board[blue_y][blue_x] == 'O' || num>10) continue;        
+        for (int i=0; i<4; i++){
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            int bny = blue_y + dy[i];
+            int bnx = blue_x + dx[i];
+            if (ny>=0 && nx>=0 && ny<N && nx<M){
+                if (board[ny][nx] != '#'){
+                    while(board[ny][nx] == '.'){
+                        ny += dy[i];
+                        nx += dx[i];                        
+                    }
+                    //red가 가능할 때만 blue도 넣어줘야한다.
+                    if (bny>=0 && bnx>=0 && bny<N && bnx<M){
+                        if (board[bny][bnx] != '#'){
+                            while(board[bny][bnx] == '.'){
+                                bny += dy[i];
+                                bnx += dx[i];
+                            }
+                            if (board[bny][bnx] == '#'){
+                                bny -= dy[i];
+                                bnx -= dx[i];                                
+                            }
+                        }
+                        else {
+                            bny = blue_y;
+                            bnx = blue_x;
+                        }
+                    }
+                    else{   //red따라 blue 옮기는게 불가능 -> 원래 자리 그대로 다시 넣기
+                        bny = blue_y;
+                        bnx = blue_x;
+                    }
+                    if (board[ny][nx] == '#'){
+                        ny -= dy[i];
+                        nx -= dx[i];
+                        if (ny == bny && nx == bnx){
+                            int r_abs = abs(y - ny) + abs(x-nx);
+                            int b_abs = abs(blue_y - bny) + abs(blue_x - bnx);
+                            if (r_abs > b_abs){
+                                ny -= dy[i];
+                                nx -= dx[i];
+                            }
+                            else{
+                                bny -= dy[i];
+                                bnx -= dx[i];
+                            }                            
+                        }
+                        cout << "ny: "<<ny<<", nx: "<<nx<<", bny: "<<bny << ", bnx: "<<bnx<<'\n';
+                        pair<int,int> idx;
+                        idx.first = ny; 
+                        idx.second = nx;                        
+                        //if (r_visited[ny][nx]) continue;    //같은 위치라도.. blue 상황에 따라 다를수있다                       
+                        r_visited[ny][nx] = true;
+                        red_q.push(make_pair(idx,num+1));                        
+                        idx.first = bny;
+                        idx.second = bnx;
+                        blue_q.push(make_pair(idx,num+1));
+                    }
+                    else if (board[ny][nx] == 'O'){
+                        if (board[bny][bnx] != 'O'){    //red가 먼저 들어감
+                            cout << num+1;
+                            exit(0);
+                        }
+                        else{   //둘 다 O에 들어간 경우 -> 실패
+
+                        }
+                    }
+                }
+            }            
+        }
+    }
+    if (ans > 10) ans = -1;
+    cout << ans;
     return 0;
 }
+
+//동시에 구멍에 빠지는 경우 처리해줘야함
